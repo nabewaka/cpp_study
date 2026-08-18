40410ff53c84d7c7a65b21db42bfbe5955022217
Wakana Manabe <manabe@MacBook-Air-von-Wakana.local>
2026-08-18 21:53:44 +0900

    初回コミット

---
+++ EX5.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b;
+  cin >> a >> b ;
+  if((0 <= a && a <= 100) && (0 <= b && b <= 100)){
+    cout << a + b << endl;
+  }
+}
